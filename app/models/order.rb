4e7c15767fe00840c72526eaf855fc08aa76ea00
Steve Lovell <sjlovell34@gmail.com>
2013-09-04 18:33:42 -0400

    finish iteration 7

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ActiveRecord::Base
   has_many :order_items, dependent: :destroy
+  belongs_to :user
 
   def total
     self.order_items.inject(0.00) { |sum, item| sum + item.product.price }
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Merchant::Application.routes.draw do
+  
   resources :orders
-
   resources :order_items
-
   resources :products
+
+  match '/auth/:provider/callback', to: 'sessions#create', via: :get
+  match '/login' => redirect('/auth/twitter'), as: :login, via: :get
+  match '/logout' => 'sessions#destroy', as: :logout, via: :get
+
+  root to: 'products#index'
 
 end
